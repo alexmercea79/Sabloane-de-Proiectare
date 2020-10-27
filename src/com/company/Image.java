ae9dc6272a2c48c2dddcbbcb0ef4292bd7676b6a
alexmercea79 <alexmercea9@gmail.com>
2020-10-27 12:01:52 +0200

    lab 5

--- src/com/company/Image.java
+++ src/com/company/Image.java
@@ -1,15 +1,22 @@
 package com.company;
 
-public class Image implements Element
-{
-    public String imageName;
+import java.util.concurrent.TimeUnit;
 
-    public Image( String ImageName)
-    {
-        this.imageName=ImageName;
+public class Image implements Element{
+
+    private String imageName;
+
+    Image(String name){
+        this.imageName=name;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    public void print()
-    {
-        System.out.println("Image with name:" + imageName);
+
+    public void print(){
+        System.out.println("Image with name: "+this.imageName);
     }
+
 }
